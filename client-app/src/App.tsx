009093415ad27e4c9826565cf3bc6a7834d4497e
Jimmy Kurian <kurianjimmy11@gmail.com>
2022-05-29 16:12:52 -0500

    feat(client-app): initial commit to add client app files

---
+++ client-app/src/App.tsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import axios from 'axios'
+
+function App() {
+  const [activities, setActivities] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/activities/')
+      .then(response => {
+        console.log(activities);
+        setActivities(response.data);
+      })
+  }, [])
+
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+       <ul>
+         {activities.map((activity: any) => ( 
+           <li key={activity.id}>{activity.name}</li>
+         ))}
+       </ul>
+      </header>
+    </div>
+  )
+}
+
+export default App
