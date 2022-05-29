f2d4f9d1e47ceb4ce8ff808b2d7272ec695258f9
Jimmy Kurian <kurianjimmy11@gmail.com>
2022-05-29 16:47:50 -0500

    chore(prettier): run formatting fix

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -4,26 +4,24 @@ import './App.css'
 import axios from 'axios'
 
 function App() {
-  const [activities, setActivities] = useState([]);
+  const [activities, setActivities] = useState([])
 
   useEffect(() => {
-    axios.get('http://localhost:5000/api/activities').then(response => {
-        console.log(response);
-        setActivities(response.data);
-      })
+    axios.get('http://localhost:5000/api/activities').then((response) => {
+      console.log(response)
+      setActivities(response.data)
+    })
   }, [])
 
   return (
     <div className='App'>
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
-       <ul>
-         {activities.map((activity: any) => ( 
-           <li key={activity.id}>
-             {activity.title}  
-           </li>
-         ))}
-       </ul>
+        <ul>
+          {activities.map((activity: any) => (
+            <li key={activity.id}>{activity.title}</li>
+          ))}
+        </ul>
       </header>
     </div>
   )
