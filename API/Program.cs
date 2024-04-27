c9855344f4f35b17b2fe7ccef1bb9b3461c8aeb7
jimmykurian <kurianjimmy11@gmail.com>
2024-04-27 19:34:56 +0300

    feat(persistence): add entityframeworkcore.sqllite and inital dbcontext

--- API/Program.cs
+++ API/Program.cs
@@ -2,12 +2,20 @@
 // Copyright (c) Jimmy Kurian. All rights reserved.
 // </copyright>
 
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to the container
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DataContext>(opt =>
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
@@ -20,37 +28,4 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching",
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index => // Removed unnecessary space to satisfy SA1025
-        new WeatherForecast(
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
-
 app.Run();
-
-/// <summary>
-/// Represents a weather forecast.
-/// </summary>
-/// <param name="date">The date of the forecast.</param>
-/// <param name="temperatureC">The temperature in Celsius.</param>
-/// <param name="summary">The summary of the weather condition.</param>
-/// Added XML comments to satisfy SA1600.
-record WeatherForecast(DateOnly date, int temperatureC, string? summary) // SA1313: Parameters renamed
-{
-    /// <summary>
-    /// Gets the temperature in Fahrenheit.
-    /// </summary>
-    public int TemperatureF => 32 + (int)(this.temperatureC / 0.5556); // Use 'temperatureC' directly, assuming use of 'this' is not applicable here as it is a value generated property.
-}
