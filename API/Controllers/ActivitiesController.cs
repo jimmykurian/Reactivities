52a9f5341a338ad702f521a2e7fbbd6be4a58fd9
jimmykurian <kurianjimmy11@gmail.com>
2024-05-06 21:55:50 +0300

    feat(api): add activities and base controller

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,49 @@
+﻿// <copyright file="ActivitiesController.cs" company="Jimmy Kurian">
+// Copyright (c) Jimmy Kurian. All rights reserved.
+// </copyright>
+
+namespace API.Controllers
+{
+    using Domain;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.EntityFrameworkCore;
+    using Persistence;
+
+    /// <summary>
+    /// Controller for managing activities.
+    /// </summary>
+    public class ActivitiesController : BaseApiController
+    {
+        private readonly DataContext context;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ActivitiesController"/> class.
+        /// </summary>
+        /// <param name="context">The data context.</param>
+        public ActivitiesController(DataContext context)
+        {
+            this.context = context;
+        }
+
+        /// <summary>
+        /// Retrieves a list of activities.
+        /// </summary>
+        /// <returns>The list of activities.</returns>
+        [HttpGet]
+        public async Task<ActionResult<List<Activity>>> GetActivities()
+        {
+            return await this.context.Activities.ToListAsync();
+        }
+
+        /// <summary>
+        /// Retrieves an activity by its ID.
+        /// </summary>
+        /// <param name="id">The ID of the activity.</param>
+        /// <returns>The activity.</returns>
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Activity>> GetActivity(Guid id)
+        {
+            return await this.context.Activities.FindAsync(id);
+        }
+    }
+}
