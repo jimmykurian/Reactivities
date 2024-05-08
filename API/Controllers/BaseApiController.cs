b911a0794f2827c22c107f454235e28b0d1a46cf
Jimmy Kurian <kurianjimmy11@gmail.com>
2024-05-08 22:57:08 +0300

    Feat/api controller into main (#4)
    * feat(api): add activities and base controller
    
    * feat(api): add controller mapping to program.cs

---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,17 @@
+﻿// <copyright file="BaseApiController.cs" company="Jimmy Kurian">
+// Copyright (c) Jimmy Kurian. All rights reserved.
+// </copyright>
+
+namespace API.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    /// <summary>
+    /// Base API controller for common API functionalities.
+    /// </summary>
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BaseApiController : ControllerBase
+    {
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -10,6 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<DataContext>(opt =>
@@ -26,7 +27,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+app.UseAuthorization();
+
+app.MapControllers();
 
 using var scope = app.Services.CreateScope();
 var services = scope.ServiceProvider;
