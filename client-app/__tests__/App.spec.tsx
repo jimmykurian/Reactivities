7593f610474e143e978f641bd0588d71eb828a42
Jimmy Kurian <kurianjimmy11@gmail.com>
2024-05-11 22:19:44 +0300

    merge: Feat/add client app into main (#6)
    * feat(client-app): intial client app commit
    
    * feat(client-app): add prettier and mods to eslint config
    
    * chore(prettier): run prettier command
    
    * feat(jest): add jest testing framework and basic unit tests

---
+++ client-app/__tests__/App.spec.tsx
@@ -0,0 +1,10 @@
+import { render, screen } from '@testing-library/react';
+import App from '../src/App';
+
+describe('App', () => {
+  test('renders Reactivities heading', () => {
+    render(<App />);
+    const headingElement = screen.getByText(/Reactivities/i);
+    expect(headingElement).toBeInTheDocument();
+  });
+});
---
+++ client-app/__tests__/main.spec.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from '../src/App';
+
+// Mock ReactDOM.createRoot and its render method
+const mockRender = jest.fn();
+jest.mock('react-dom/client', () => ({
+  createRoot: jest.fn().mockImplementation(() => ({
+    render: mockRender,
+  })),
+}));
+
+describe('main.tsx', () => {
+  it('renders the App component', async () => {
+    const root = document.createElement('div');
+    root.id = 'root';
+    document.body.appendChild(root);
+
+    // Dynamically import the main module
+    await import('../src/main');
+
+    expect(root).toBeInTheDocument();
+    expect(ReactDOM.createRoot).toHaveBeenCalledWith(root);
+    expect(mockRender).toHaveBeenCalledWith(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>,
+    );
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks(); // Clear mocks to avoid state leakage between tests
+  });
+});
