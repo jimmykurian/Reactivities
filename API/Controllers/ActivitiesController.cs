649c4938b6d98b9e6ec18976c4e026c6182c331e
Jimmy Kurian <kurianjimmy11@gmail.com>
2024-05-18 17:02:00 +0300

    feat(application): add mediatr and list class (#16)
    Add list class (query and handler) to Application project. Add MediatR nuget package. Update base Controller.

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -4,27 +4,15 @@
 
 namespace API.Controllers
 {
+    using Application.Activities;
     using Domain;
     using Microsoft.AspNetCore.Mvc;
-    using Microsoft.EntityFrameworkCore;
-    using Persistence;
 
     /// <summary>
     /// Controller for managing activities.
     /// </summary>
     public class ActivitiesController : BaseApiController
     {
-        private readonly DataContext context;
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="ActivitiesController"/> class.
-        /// </summary>
-        /// <param name="context">The data context.</param>
-        public ActivitiesController(DataContext context)
-        {
-            this.context = context;
-        }
-
         /// <summary>
         /// Retrieves a list of activities.
         /// </summary>
@@ -32,7 +20,7 @@ namespace API.Controllers
         [HttpGet]
         public async Task<ActionResult<List<Activity>>> GetActivities()
         {
-            return await this.context.Activities.ToListAsync();
+            return await this.Mediator.Send(new List.Query());
         }
 
         /// <summary>
@@ -43,7 +31,7 @@ namespace API.Controllers
         [HttpGet("{id}")]
         public async Task<ActionResult<Activity>> GetActivity(Guid id)
         {
-            return await this.context.Activities.FindAsync(id);
+            return await Task.FromResult(this.Ok());
         }
     }
 }
